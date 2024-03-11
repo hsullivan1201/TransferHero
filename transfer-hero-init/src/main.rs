48b9913402c928376456b727933827df01f3c7ca
hsullivan1201 <jsulliva1201@gmail.com>
2024-03-10 20:37:55 -0400

    fixes

--- transfer-hero-init/src/main.rs
+++ transfer-hero-init/src/main.rs
@@ -1,36 +1,25 @@
-use reqwest::Response;
-use std::io::prelude::*;
-use std::io;
-use std::num::ParseIntError;
+use reqwest::header;
+use rocket::{get, launch, routes};
 
-#[macro_use] extern crate rocket;
-
-
-#[get("/api.wmata.com/StationPrediction.svc/json/GetPrediction/B03")]
-
-async fn request() -> io::Result<Response>{
-    use reqwest::header;
-    let mut headers = header::HeaderMap::new();
-    headers.insert("X-MY-HEADER", header::HeaderValue::from_static("value"));
-
-    // Consider marking security-sensitive headers with `set_sensitive`.
-    let mut auth_value = header::HeaderValue::from_static("secret");
-    auth_value.set_sensitive(true);
-    headers.insert(header::AUTHORIZATION, auth_value);
-
-    // get a client builder
-    let client = reqwest::Client::builder()
-        .default_headers(headers)
-        .build()?;
-    let res = client.get("https://www.rust-lang.org").send().await?;
-    Ok(())
-}
-
-fn index() -> &'static str {
+#[get("/")]
+async fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/make_request")]
+async fn make_request() -> Result<String, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let res = client.get("https://api.wmata.com/StationPrediction.svc/json/GetPrediction/B03")
+        .header("X-MY-HEADER", "value")
+        .header("Authorization", "Bearer secret")
+        .send()
+        .await?;
+
+    let body = res.text().await?;
+    Ok(body)
+}
+
 #[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+async fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, make_request]).await
 }
